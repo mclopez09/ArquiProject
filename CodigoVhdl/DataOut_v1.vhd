e224c08f6fdde5f4caf611c9ac35490f79c6c949
Germanc97 <germanc97@javerianacali.edu.co>
2018-11-15 03:53:51 -0500

    Add files via upload

--- CodigoVhdl/DataOut_v1.vhd
+++ CodigoVhdl/DataOut_v1.vhd
@@ -19,7 +19,7 @@ use IEEE.std_logic_unsigned.all;
 Entity DataOut_v1 is
 	generic(NdataOut: positive :=8);
 	port
-	(input: in std_logic_vector(NdataOut-1 downto 0);
+	(entrada: in std_logic_vector(NdataOut-1 downto 0);
 	out1: out std_logic_vector(NdataOut-1 downto 0);
 	out2: out std_logic_vector(NdataOut-1 downto 0);
 	out3 : out std_logic_vector(NdataOut-1 downto 0);
@@ -31,16 +31,18 @@ Architecture DataOut_arc_v1 of DataOut_v1 is
 signal diez: integer:=10;
 signal tmp: integer;
 signal tmp2:integer;
+signal tmp3:integer;
+signal tmp4:integer;
 begin
-	process(input)is
+	process(entrada)is
 	begin
-		tmp <=conv_integer(input);
+		tmp <=conv_integer(entrada);
 		tmp2 <=tmp mod	diez;
-		tmp <= tmp-tmp2;
-		out2 <= std_logic_vector(to_unsigned(tmp2, out2'length));
-		if tmp > diez then
-			tmp <= tmp/diez;	
-			out1 <= std_logic_vector(to_unsigned(tmp, out1'length));
+		tmp3 <= tmp-tmp2;
+		out2 <= std_logic_vector(to_unsigned(tmp2, NdataOut));
+		if tmp3 > diez then
+			tmp4 <= tmp/diez;	
+			out1 <= std_logic_vector(to_unsigned(tmp4, NdataOut));
 		end if;
 	end process;
 end architecture;
