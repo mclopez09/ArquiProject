dfbc46a0af410ea1f43db6b3eb98477c36f043da
David Hernández Cárdenas <davidher28@javerianacali.edu.co>
2018-11-20 20:02:22 -1000

    Add files via upload

--- CodigoVhdl/IR_v1.vhd
+++ CodigoVhdl/IR_v1.vhd
@@ -23,13 +23,20 @@ Entity IR_v1 is
 		oeir,oeirj,irwrite: inout std_logic;
 		clk : in std_logic;
 		opcode: out std_logic_vector(Mir-1 downto 0);
-		outrd,outrs,outrt : out std_logic_vector(Mir-1 downto 0):="0000";
-		outjump, constante, constante_dir : out std_logic_vector(Pir-1 downto 0)
+		opcode2: out std_logic_vector(Mir-1 downto 0);
+		opcode3: out std_logic_vector(Mir-1 downto 0);
+		outrd,outrs,outrt : out std_logic_vector(Mir-1 downto 0);
+		outrd2,outrs2,outrt2 : out std_logic_vector(Mir-1 downto 0);
+		outjump, constante, constante_dir : out std_logic_vector(Pir-1 downto 0);
+		outjump2, constante2, constante_dir2 : out std_logic_vector(Pir-1 downto 0)
 	);
 end entity;
 Architecture IR_v1_arc of IR_v1 is
 signal opcodetmp : std_logic_vector(3 downto 0);
 signal opcodetmp2 : std_logic_vector(3 downto 0);
+signal tmp: std_logic_vector(Pir-1 downto 0);
+signal tmp2: std_logic_vector(Pir-1 downto 0);
+signal tmp3: std_logic_vector(Pir-1 downto 0);
 	Begin
 	process(clk,irwrite)
 	Begin
@@ -42,6 +49,8 @@ signal opcodetmp2 : std_logic_vector(3 downto 0);
 		opcodetmp2(1)<=instruccion(17);
 		opcodetmp2(2)<=instruccion(18);
 		opcodetmp2(3)<=instruccion(19);
+		opcode2<=opcodetmp;
+		opcode3<=opcodetmp2;
 		
 	end if;
 	end process;
@@ -64,37 +73,42 @@ signal opcodetmp2 : std_logic_vector(3 downto 0);
 				opcode<=opcodetmp;
 			elsif (opcodetmp2="1010" ) then
 				oeirj <= '1';
-				outjump(0)<=instruccion(0);
-				outjump(1)<=instruccion(1);
-				outjump(2)<=instruccion(2);
-				outjump(3)<=instruccion(3);
-				outjump(4)<=instruccion(4);
-				outjump(5)<=instruccion(5);
-				outjump(6)<=instruccion(6);
-				outjump(7)<=instruccion(7);
+				tmp3(0)<=instruccion(0);
+				tmp3(1)<=instruccion(1);
+				tmp3(2)<=instruccion(2);
+				tmp3(3)<=instruccion(3);
+				tmp3(4)<=instruccion(4);
+				tmp3(5)<=instruccion(5);
+				tmp3(6)<=instruccion(6);
+				tmp3(7)<=instruccion(7);
 				opcode<=opcodetmp2;
+				outjump<=tmp3;
+				outjump2<=tmp3;
 				
 			elsif (opcodetmp2="0001") then
-				constante(0)<=instruccion(0);
-				constante(1)<=instruccion(1);
-				constante(2)<=instruccion(2);
-				constante(3)<=instruccion(3);
-				constante(4)<=instruccion(4);
-				constante(5)<=instruccion(5);
-				constante(6)<=instruccion(6);
-				constante(7)<=instruccion(7);
+				tmp(0)<=instruccion(0);
+				tmp(1)<=instruccion(1);
+				tmp(2)<=instruccion(2);
+				tmp(3)<=instruccion(3);
+				tmp(4)<=instruccion(4);
+				tmp(5)<=instruccion(5);
+				tmp(6)<=instruccion(6);
+				tmp(7)<=instruccion(7);
 				opcode<=opcodetmp2;
+				constante<=tmp;
+				constante2<=tmp2;
+
 					
-			elsif(opcodetmp2="0100" or opcodetmp2="0101" or opcodetmp2="0110" or opcodetmp2="0111" or opcodetmp2="1000" or opcodetmp2="1001" or opcodetmp2="1101" or opcodetmp2="1110") then
+			elsif(opcodetmp2="0100" or opcodetmp2="0101" or opcodetmp2="0110" or opcodetmp2="0111" or opcodetmp2="1000" or opcodetmp2="1001" or opcodetmp2="1101" or opcodetmp2="1110" or opcodetmp2="1011" or opcodetmp2="1100") then
 				oeir <= '1';
-				constante_dir(0)<=instruccion(0);
-				constante_dir(1)<=instruccion(1);
-				constante_dir(2)<=instruccion(2);
-				constante_dir(3)<=instruccion(3);
-				constante_dir(4)<=instruccion(4);
-				constante_dir(5)<=instruccion(5);
-				constante_dir(6)<=instruccion(6);
-				constante_dir(7)<=instruccion(7);
+				tmp2(0)<=instruccion(0);
+				tmp2(1)<=instruccion(1);
+				tmp2(2)<=instruccion(2);
+				tmp2(3)<=instruccion(3);
+				tmp2(4)<=instruccion(4);
+				tmp2(5)<=instruccion(5);
+				tmp2(6)<=instruccion(6);
+				tmp2(7)<=instruccion(7);
 				outrt(0)<=instruccion(8);
 				outrt(1)<=instruccion(9);
 				outrt(2)<=instruccion(10);
@@ -104,6 +118,8 @@ signal opcodetmp2 : std_logic_vector(3 downto 0);
 				outrs(2)<=instruccion(14);
 				outrs(3)<=instruccion(15);
 				opcode<=opcodetmp2;
+				constante_dir<=tmp2;
+				constante_dir2<=tmp2;
 			else 
 				outjump<=NULL;
 				outrd<=NULL;
